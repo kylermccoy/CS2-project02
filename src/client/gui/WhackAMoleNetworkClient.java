9070a09c1ada9d9c819b030bf54e1935c7fc276c
Alex Cooley <arc7311@rit.edu>
2019-04-12 12:42:58 -0400

    started the network client, opened the sockets and input/output

--- src/client/gui/WhackAMoleNetworkClient.java
+++ src/client/gui/WhackAMoleNetworkClient.java
@@ -1,4 +1,38 @@
 package client.gui;
 
+import common.WhackAMoleBoard;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class WhackAMoleNetworkClient {
+    private Socket clientSocket;
+    private Scanner networkIn;
+    private PrintStream networkOut;
+    private WhackAMoleBoard board;
+    private boolean loopcontrol;
+
+    public WhackAMoleNetworkClient(String host, int port, WhackAMoleBoard board){
+        try{
+            this.clientSocket = new Socket(host, port);
+            this.networkIn = new Scanner(clientSocket.getInputStream());
+            this.networkOut = new PrintStream(clientSocket.getOutputStream());
+            this.board = board;
+            //might need to throw if the protocol isnt CONNECT
+
+        }
+        catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    private synchronized boolean gameContinue(){return this.loopcontrol;}
+
+    private synchronized void stop(){this.loopcontrol = false;}
+
+    //will fix when the board is created
+    private void makeMove(){this.board.makeMove();}
+
 }
